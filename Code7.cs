5d10ff78a0f2e489c324f6bdf58d8fce17e88ad7
degtev.2023 <degtev.2023@stud.nstu.ru>
2026-05-18 16:22:48 +0000

    v1

---
+++ Code7.cs
@@ -0,0 +1,142 @@
+// ctrl k, ctrl c, ctrl u
+
+// данные типа int, float, массивы из элементов указанных типов; 
+// инструкции описания переменных;
+// операторы присваивания в любой последовательности;
+// операции  +, – ,  *, = =, != , <, > 
+
+// Неполное ;
+int z1 = ;
+int z1 = 2
+int z3 3;
+
+// Имена идентификаторов состоят из символов алфавита
+// int
+int 1i = 1;
+int i_1 = 1;
+int 1_i = 1;
+int i.1 = 1;
+int 1.i = 1;
+int i,1 = 1;
+int 1,i = 1;
+
+// float
+float 1f = 1.1;
+float f_1 = 1.1;
+float 1_f = 1.1;
+float f.1 = 1.1;
+float 1.f = 1.1;
+float f,1 = 1.1;
+float 1,f = 1.1;
+
+// массивы
+int[] intm = new int[4] { 1, 2, 3, 5 };
+int[] intm = new int[] { 1, 2, 3, 5 };
+int[] intm = { 1, 2, 3, 5 };
+intm[3] = intm[3] + 30;
+print(intm);
+
+int[] intm1 = new int[4.5] { 1, 2, 3, 5 };
+
+float[] intf = new float[4] { 1.1, 2.1, 3.1, 5.1 };
+float[] intf = new float[] { 1.1, 2.1, 3.1, 5.1 };
+float[] intf = { 1.1, 2.1, 3.1, 5.1 };
+intf[3] = intf[3] * 2;
+print(intf);
+
+// Проверка констант
+PI = 3;
+.PI = 3;
+_PI = 3;
+!PI = 3;
+$PI = 3;
+
+Pi = 3;
+.Pi = 3;
+_Pi = 3;
+!Pi = 3;
+$Pi = 3;
+
+int PI = 3;
+int.PI = 3;
+int _PI = 3;
+int !PI = 3;
+int $PI = 3;
+
+int Pi = 3;
+int.Pi = 3;
+int _Pi = 3;
+int !Pi = 3;
+int $Pi = 3;
+
+const int PI = 3;
+const int .PI = 3;
+const int _PI = 3;
+const int !PI = 3;
+const int $PI = 3;
+
+const int Pi = 3;
+const int .Pi = 3;
+const int _Pi = 3;
+const int !Pi = 3;
+const int $Pi = 3;
+
+int MAX_VALUE = 10;
+MAX_VALUE = 30;
+
+int Max_VALUE = 10;
+													
+// Идентификатор или константа не должны быть зарезервированным словом. 													
+int main = 1;
+int int = 2;
+int float = 2.1;
+
+// Синтаксические ошибки
+// Повторное объявление переменной.
+int u1 = 1;
+int u1 = 3;
+
+// Предложение и неполное
+int q1 = 1; int
+q2 = 3;
+
+// Два полных предложения
+int q3 = 1; int q4 = 3;
+
+// Проверки на соответствие заявленного типа и значения переменной этого типа.
+int s1 = 'c';
+int s2 = 1.1;
+int s3 = 'char';
+float s6 = 'c';
+float s7 = 1;
+
+int[] mass = new int[3] { 1, 3, 5.1 };
+int[] mass = new int[] { 1, 3, 5.1 };
+int[] mass = { 1, 3, 5.1 };
+print(mass);
+
+// Математические операции
+int num_i1 = 1;
+int num_i2 = 1;
+float num_f = 3.5;
+
+int plus = num_i1 + num_i2;
+int minus = num_i1 - num_i2;
+int mult = num_i1 * num_i2;
+
+int plus1 = num_i1 + num_f;
+int minus1 = num_i1 - num_f;
+int mult1 = num_i1 * num_f;
+
+// Комментарий
+
+// Закрытый комментарий
+/*
+	Закрытый комментарий
+*/
+
+// Неполный комментарий
+/ Неполный комментарий
+
+// Незакрытый комментарий
+/* Незакрытый комментарий
